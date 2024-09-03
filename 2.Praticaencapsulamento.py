03be5c8dcd89fbb3a74639047e51f5d28a6cc331
mascarenhas2 <160731737+mascarenhas2@users.noreply.github.com>
2024-09-03 20:18:13 +0000

    Corrigir: Praticaencapslamento

--- 2.Praticaencapsulamento.py
+++ 2.Praticaencapsulamento.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractmethod
 import os
 
+#criando erros
+class SalarioNegativoError(Exception):
+    pass
+
 os.system("cls || clear")
 
 class Endereco:
@@ -14,14 +18,46 @@ class Endereco:
                 f"\nNumero: {self.numero}"
                 f"\nCidade: {self.cidade}"        
                  )
+    def exibir_endereco(self) -> str:
+        return f"Logradouro: {self.logradouro} \nNumero: {self.numero} \nCidade: {self.cidade}"
     
 class Funcionario(ABC):
     def __init__(self, nome: str, email : str, salario : float, endereco: Endereco) -> None:
+        self.nome = nome
+        self.email = email
+        self.salario = salario
+        self.endereco = endereco
+    
+    @abstractmethod
+    def salario_final(self) -> float:
+        pass
+
+    def __str__(self) -> str:  
         return (f"\nNome: {self.nome}"
                 f"\nEmail: {self.email}"
                 f"\nSalario: {self.salario}"
-                f"\nEndereco: {self.endereco}"
-                )
-    
-    @abstractmethod
-    def salario                                             
+                f"\nEndereco: {self.endereco.exibir_endereco()}")
+                
+class Gerente(Funcionario):
+    def __init__(self, nome: str, email: str, salario: float, endereco: Endereco) -> None:
+        super().__init__(nome, email, salario, endereco) 
+
+    def __str__(self) -> str:
+        return f"\nNome: {self.nome} \nEmail: {self.email} \nSalario: {self.salario} Endereco: {self.endereco.exibir_endereco()}"     
+
+class Motoboy(Funcionario):
+    def __init__(self, nome: str,cnh: str, email: str, salario: float, endereco: Endereco) -> None:
+        super().__init__(nome, email, salario, endereco)
+        self.cnh = cnh
+
+    def __str__(self) -> str:
+        return f"\nNome: {self.nome} \nemail: {self.email} \nSalario: {self.salario} \nEndereco: {self.endereco.exibir_endereco}"
+
+#instanciando os objetos
+gerente1 = Gerente("william", "willa@gmail.com",4000,
+                   Endereco("Rua sugas",23,"Salvador"))
+print(gerente1)
+
+motoboy1 = Motoboy("Lucas","A","Vander@gmail.com",-1000,
+                   Endereco("Rua ponciano",23,"Salvador"))
+print(motoboy1)
