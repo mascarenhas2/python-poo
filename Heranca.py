e890e1aaa728d3fb94cdb013909205e1522df62c
mascarenhas2 <160731737+mascarenhas2@users.noreply.github.com>
2024-09-03 11:01:41 +0000

    alteracao

--- Heranca.py
+++ Heranca.py
@@ -1,42 +1,45 @@
 from abc import ABC, abstractmethod
 import os
-
-os.system("cls || clear") #limpa terminal.
+os.system("cls || clear")
 
 class Funcionario(ABC):
-    def __init__ (self, nome:str, idade:int, salario: float) -> None:
-        self.nome = nome 
+    def __init__(self, nome: str, idade: int, salario: float) -> None:
+        self.nome = nome
         self.idade = idade
-        self.salario = salario 
+        self.salario = salario
 
-    @abstractmethod    
+    @abstractmethod
     def calcular_salario(self) -> float:
-        pass    
+        pass
 
 class Gerente(Funcionario):
     def calcular_salario(self) -> float:
-            # Acrescimo de 20%
-            BONIFICACAO = 1.1
-            salarioFinal = self.salario * BONIFICACAO
-            return salarioFinal
-    def __str__(self) -> str:
-         return f"nome: {self.nome} idade: {self.idade} salario:{self.salario}
-        
-class Motoboy(Funcionario):
-    def __init__(self, nome: str, cnh: str ,idade: int, salario: float) -> None:
-        super().__init__(nome, idade, salario)
-        self.cnh = cnh
-    def calcular_salario(self) -> float:
-        # Acrescimo de 20%
+        # Acréscimo de 10%
         BONIFICACAO = 1.1
         salarioFinal = self.salario * BONIFICACAO
         return salarioFinal
+
     def __str__(self) -> str:
-         return f"nome: {self.nome} idade: {self.idade} Salario: {self.calcular_salario()}"
-        
+        return f"Nome: {self.nome}\nIdade: {self.idade}\nSalário: {self.calcular_salario():.2f}"
 
-gerente1 =  Gerente("Claudio",23,1000.0)
-motoboy1 =  Motoboy("junior","2343434",20,500.0)
+class Motoboy(Funcionario):
+    def __init__(self, nome: str, cnh: str, idade: int, salario: float) -> None:
+        super().__init__(nome, idade, salario)
+        self.cnh = cnh
 
+    def calcular_salario(self) -> float:
+        # Acréscimo de 10%
+        BONIFICACAO = 1.1
+        salarioFinal = self.salario * BONIFICACAO
+        return salarioFinal
+
+    def __str__(self) -> str:
+        return f"\nNome: {self.nome}\nIdade: {self.idade}\nSalário: {self.calcular_salario():.2f}\nCNH: {self.cnh}"
+
+# Instanciando os objetos
+gerente1 = Gerente("Claudio", 23, 1000.0)
+motoboy1 = Motoboy("Junior", "2343434", 20, 500.0)
+
+# Exibindo as informações
 print(gerente1)
 print(motoboy1)
---
+++ projeto1.py
@@ -0,0 +1,4 @@
+from abc import  ABC, abstractmethod
+import os
+os.system("cls || clear")
+
